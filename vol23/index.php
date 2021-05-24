92f95c9e7f38643e167c9b2e698b0d15d9cda71a
gray-code <koheiishido@gmail.com>
2021-05-25 01:14:16 +0900

    [Edit] Cleaning code

--- vol23/index.php
+++ vol23/index.php
@@ -3,95 +3,112 @@
 // データベースの接続情報
 define( 'DB_HOST', 'localhost');
 define( 'DB_USER', 'root');
-define( 'DB_PASS', 'password');
+define( 'DB_PASS', 'dogman0807');
 define( 'DB_NAME', 'board');
 
 // タイムゾーン設定
 date_default_timezone_set('Asia/Tokyo');
 
 // 変数の初期化
-$now_date = null;
-$data = null;
-$file_handle = null;
-$split_data = null;
+$current_date = null;
+$message = array();
 $message_array = array();
 $error_message = array();
-$clean = array();
+$pdo = null;
+$stmt = null;
+$res = null;
+$option = null;
 
 session_start();
 
+// データベースに接続
+try {
+
+    $option = array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::MYSQL_ATTR_MULTI_STATEMENTS => false
+    );
+    $pdo = new PDO('mysql:charset=UTF8;dbname='.DB_NAME.';host='.DB_HOST , DB_USER, DB_PASS, $option);
+
+} catch(PDOException $e) {
+
+    // 接続エラーのときエラー内容を取得する
+    $error_message[] = $e->getMessage();
+}
+
 if( !empty($_POST['btn_submit']) ) {
-	
+
+    // 空白除去
+	$view_name = preg_replace( '/\A[\p{C}\p{Z}]++|[\p{C}\p{Z}]++\z/u', '', $_POST['view_name']);
+	$message = preg_replace( '/\A[\p{C}\p{Z}]++|[\p{C}\p{Z}]++\z/u', '', $_POST['message']);
+
 	// 表示名の入力チェック
-	if( empty($_POST['view_name']) ) {
+	if( empty($view_name) ) {
 		$error_message[] = '表示名を入力してください。';
 	} else {
-		$clean['view_name'] = htmlspecialchars( $_POST['view_name'], ENT_QUOTES);
 
-		// セッションに表示名を保存
-		$_SESSION['view_name'] = $clean['view_name'];
-	}
-	
+        // セッションに表示名を保存
+		$_SESSION['view_name'] = $view_name;
+    }
+
 	// メッセージの入力チェック
-	if( empty($_POST['message']) ) {
+	if( empty($message) ) {
 		$error_message[] = 'ひと言メッセージを入力してください。';
-	} else {
-		$clean['message'] = htmlspecialchars( $_POST['message'], ENT_QUOTES);
 	}
 
 	if( empty($error_message) ) {
 		
-		// データベースに接続
-		$mysqli = new mysqli( DB_HOST, DB_USER, DB_PASS, DB_NAME);
+		// 書き込み日時を取得
+        $current_date = date("Y-m-d H:i:s");
 
-		// 接続エラーの確認
-		if( $mysqli->connect_errno ) {
-			$error_message[] = '書き込みに失敗しました。 エラー番号 '.$mysqli->connect_errno.' : '.$mysqli->connect_error;
-		} else {
+        // トランザクション開始
+        $pdo->beginTransaction();
 
-			// 文字コード設定
-			$mysqli->set_charset('utf8');
-			
-			// 書き込み日時を取得
-			$now_date = date("Y-m-d H:i:s");
-			
-			// データを登録するSQL作成
-			$sql = "INSERT INTO message (view_name, message, post_date) VALUES ( '$clean[view_name]', '$clean[message]', '$now_date')";
-			
-			// データを登録
-			$res = $mysqli->query($sql);
-		
-			if( $res ) {
-				$_SESSION['success_message'] = 'メッセージを書き込みました。';
-			} else {
-				$error_message[] = '書き込みに失敗しました。';
-			}
-		
-			// データベースの接続を閉じる
-			$mysqli->close();
-		}
+        try {
 
-		header('Location: ./');
+            // SQL作成
+            $stmt = $pdo->prepare("INSERT INTO message (view_name, message, post_date) VALUES ( :view_name, :message, :current_date)");
+
+            // 値をセット
+            $stmt->bindParam( ':view_name', $view_name, PDO::PARAM_STR);
+            $stmt->bindParam( ':message', $message, PDO::PARAM_STR);
+            $stmt->bindParam( ':current_date', $current_date, PDO::PARAM_STR);
+
+            // SQLクエリの実行
+            $res = $stmt->execute();
+
+            // コミット
+            $res = $pdo->commit();
+
+        } catch(Exception $e) {
+
+            // エラーが発生した時はロールバック
+            $pdo->rollBack();
+        }
+
+        if( $res ) {
+            $_SESSION['success_message'] = 'メッセージを書き込みました。';
+        } else {
+            $error_message[] = '書き込みに失敗しました。';
+        }
+
+        // プリペアドステートメントを削除
+        $stmt = null;
+
+        header('Location: ./');
+        exit;
 	}
 }
 
-// データベースに接続
-$mysqli = new mysqli( DB_HOST, DB_USER, DB_PASS, DB_NAME);
+if( !empty($pdo) ) {
 
-// 接続エラーの確認
-if( $mysqli->connect_errno ) {
-	$error_message[] = 'データの読み込みに失敗しました。 エラー番号 '.$mysqli->connect_errno.' : '.$mysqli->connect_error;
-} else {
-
-	$sql = "SELECT view_name,message,post_date FROM message ORDER BY post_date DESC";
-	$res = $mysqli->query($sql);
-
-    if( $res ) {
-		$message_array = $res->fetch_all(MYSQLI_ASSOC);
-    }
-
-    $mysqli->close();
+    // メッセージのデータを取得する
+    $sql = "SELECT view_name,message,post_date FROM message ORDER BY post_date DESC";
+    $message_array = $pdo->query($sql);
 }
+
+// データベースの接続を閉じる
+$pdo = null;
 
 ?>
 <!DOCTYPE html>
@@ -379,7 +396,7 @@ article.reply::before {
 <body>
 <h1>ひと言掲示板</h1>
 <?php if( empty($_POST['btn_submit']) && !empty($_SESSION['success_message']) ): ?>
-    <p class="success_message"><?php echo $_SESSION['success_message']; ?></p>
+    <p class="success_message"><?php echo htmlspecialchars( $_SESSION['success_message'], ENT_QUOTES, 'UTF-8'); ?></p>
     <?php unset($_SESSION['success_message']); ?>
 <?php endif; ?>
 <?php if( !empty($error_message) ): ?>
@@ -392,7 +409,7 @@ article.reply::before {
 <form method="post">
 	<div>
 		<label for="view_name">表示名</label>
-		<input id="view_name" type="text" name="view_name" value="<?php if( !empty($_SESSION['view_name']) ){ echo $_SESSION['view_name']; } ?>">
+		<input id="view_name" type="text" name="view_name" value="<?php if( !empty($_SESSION['view_name']) ){ echo htmlspecialchars( $_SESSION['view_name'], ENT_QUOTES, 'UTF-8'); } ?>">
 	</div>
 	<div>
 		<label for="message">ひと言メッセージ</label>
